8babc45e360f1485d6b926a59ba48c9b1fa11a9b
user.email <stephanie.enciso13@gmail.com>
2020-12-17 17:34:07 -0600

    Passed the tests for name, email, password, terms of service, and submit.

---
+++ cypress/integration/examples/form_test.js
@@ -0,0 +1,52 @@
+describe("User Form App", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
+    });
+
+    const nameInput = () => cy.get('input[name = "name"]');
+    const emailInput = () => cy.get('input[name = "email"]');
+    const passwordInput = () => cy.get('input[name = "password"]');
+    const termsInput = () => cy.get('input[name = "terms"]');
+    const submitBtn = () => cy.get('button');
+
+    it('testing to make sure everything is working.', () => {
+        expect(true).to.equal(true);
+
+    });
+
+    it('Can type in the name input', () => {
+        nameInput()
+         .should('have.value', '')
+         .type('Anime')
+         .should('have.value', 'Anime');   
+    });
+
+    it('Can type in the email input', () => {
+        emailInput()
+        .should('have.value', '')
+        .type('anime@CuteLilDoggie.com')
+        .should('have.value', 'anime@CuteLilDoggie.com');
+    });
+
+    it('Can type in the password input', () => {
+        passwordInput()
+         .should('have.value', '')
+         .type('AdorablePup')
+         .should('have.value', 'AdorablePup')
+    });
+
+    it('Can check Terms of Service box', () => {
+        termsInput().check();
+    });
+
+    it('Can submit the form', () => {
+        nameInput().type('Anime');
+        emailInput().type('anime@CuteLilDoggie.com');
+        passwordInput().type('AdorablePup');
+        termsInput().check();
+        submitBtn().click();
+
+    })
+
+
+});
